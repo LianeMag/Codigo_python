46dec7d3bd569bec4f98e48d245d69e0e1d87e84
Liane Magalhães <liane.mag31@gmail.com>
2023-09-27 01:59:19 -0300

    01

---
+++ Codigo01.py
@@ -0,0 +1,7 @@
+a = input('digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um número? ' ,a.isnumeric())
+print('É alfabético? ',a.isalpha())
+print('É Alfanumérico? ', a.isalnum())
+print('É um digito?  ', a.isdigit())
